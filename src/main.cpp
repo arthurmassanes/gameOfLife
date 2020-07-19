2802d3ce34db5f7ef8e1f2034991b3a9293f93bd
arthur <arthur.massanes@epitech.eu>
2020-07-20 00:17:35 +0200

    added -h

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,24 @@
 #include "Board.hpp"
 #include "Game.hpp"
 
+int printHelp(void)
+{
+    std::cout << "\tgameOfLife" << "\n";
+    std::cout << "USAGE\n./gameOflife [-h] [MAP_FILE]";
+    std::cout << "\n\n\tMAP_FILE\tThe file containing cells\n";
+    std::cout << "\t-h\t\tDisplay usage" << "\n";
+    return (0);
+}
+
+bool isHelpOption(int ac, char **av)
+{
+    return (ac > 1 && std::string(av[1]) == "-h");
+}
+
 int main(int ac, char **av)
 {
+    if (isHelpOption(ac, av))
+        return (printHelp());
     try {
         sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), NAME);
         Game g(&window, ac > 1 ? av[1] : DEFAULT_MAP);
